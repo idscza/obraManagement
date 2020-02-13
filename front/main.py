204d1f2e9a241794fb9681e3d9ba0ad77505423c
Cupi2 <id.salazar@uniandes.edu.co>
2020-02-13 15:34:10 -0500

    Funcionan las transacciones

--- front/main.py
+++ front/main.py
@@ -19,9 +19,10 @@ def_values = ('Los Alpes','Real Danés','Atalayas')
 def __main__():
     window = Tk()
     window.title("Manejo de Presupuesto")
-    window.geometry('915x270')
+    window.geometry('945x270')
 
-
+    global def_values
+    mu.generate_mockups(def_values)
 
     lbl = Label(window, text="Bienvenidos al Manejador \nde Presupuestos de Obras",font=("Arial",20))
     lbl.grid(column=0, row=1)
@@ -29,17 +30,36 @@ def __main__():
     lbl1 = Label(window, text=' ')
     lbl1.grid(column=1,row=0)
     
+    lbl2 = Label(window, text='         ')
+    lbl2.grid(column=1,row=1)
+    
     txt = scrolledtext.ScrolledText(window,width=70,height=10)
-    txt.grid(column=1, row=1)
+    txt.grid(column=2, row=1)
     
-    btn1 = Button(window, text="Registrar Transacción")
-    btn1.grid(column=1, row=2)
+ 
     
-    btn2 = Button(window, text="Ver reporte")
-    btn2.grid(column=1, row=3)
+    monto = Entry(window,width=25,state='disabled')
+    monto.grid(column=2, row=2, sticky = (E))
+    
+    combochoice = Combobox(window,width=25)
+    combochoice.grid(column=2, row=2)
+    
+    
+    btn2 = Button(window, text="Agregar Rubro            ",state='disabled')
+    btn2.grid(column=2, row=3, sticky = (W))
+    
+    nomrub = Entry(window,width=28,state='disabled')
+    nomrub.grid(column=2, row=3)
+    
+    cant = Entry(window,width=25,state='disabled')
+    cant.grid(column=2, row=3, sticky = (E))
+    
+    
+    '''btn3 = Button(window, text="Ver Reporte",state='disabled')
+    btn3.grid(column=2, row=3, sticky = (W))'''
      
     combo = Combobox(window,width=32)
-    forz_act = partial(actualizar_valores,[combo,txt])
+    forz_act = partial(actualizar_valores,[combo,txt,combochoice])
     window.bind('<<ComboboxSelected>>', forz_act)
 
     combo.grid(column = 0, row = 3, sticky = (E))
@@ -51,16 +71,21 @@ def __main__():
     nomobra = Entry(window,width=35,state='disabled')
     nomobra.grid(column=0, row=2, sticky = (E))
     
-    agob=partial(agregar_obra,[combo,txt,nomobra])
+    agob=partial(agregar_obra,[combo,txt,combochoice,nomobra])
     btn = Button(window, text="Agregar\n  Obra",command=agob,state='disabled')
     btn.grid(column=0, row=2, sticky = (W))
+    
+    
+    regtran=partial(registrar_transaccion,[combo,txt,combochoice,monto])
+    btn1 = Button(window, text="Registrar Transacción",command=regtran,state='disabled')
+    btn1.grid(column=2, row=2, sticky = (W))
     
     
     menus = Menu(window)
  
     new_item = Menu(menus)
 
-    cheat = partial(cargar_obras,[combo,txt,btn,nomobra])   
+    cheat = partial(cargar_obras,[combo,txt,combochoice,monto,btn,nomobra,btn1,btn2,nomrub,cant])   
     new_item.add_command(label='Cargar Obras',command=cheat)
     #new_item.add_command(label='Seleccionar Obra')
     
@@ -69,7 +94,7 @@ def __main__():
     
     
     window.config(menu=menus) 
-    
+    window.resizable(False, False)
     window.mainloop()
 
 #Totally Mockup, it should call SQL DB
@@ -77,19 +102,14 @@ def cargar_obras(data):
     global def_values
     data[0]['values'] = def_values
     data[0].current(0)
-    data[2].state(['!disabled'])
+    data[6].state(['!disabled'])
+    data[7].state(['!disabled'])
+    data[8].state(['!disabled'])
+    data[9].state(['!disabled'])
+    data[4].state(['!disabled'])
+    data[5].state(['!disabled'])
     data[3].state(['!disabled'])
     actualizar_valores(data,'')
-'''    
-def actualizar_obras(combob,val):
-    tup = combob['values']
-    tupact = []
-    for i in tup:
-        tupact.add(i)
-    tupact.add(val)
-    tupact = tuple(tupact)
-    combob['values'] = tupact
-    '''
 
 def actualizar_valores(data,x):
     name = data[0].get()
@@ -97,7 +117,7 @@ def actualizar_valores(data,x):
     corpus = ''
     
     corpus = name
-    mud = mu.generate_mockup(name)
+    mud = mu.get_mockup(name)
     
     global actual
     actual = mud
@@ -105,22 +125,36 @@ def actualizar_valores(data,x):
     mud1 = {'Entradas':mud['Entradas'],
             'Salidas':mud['Salidas']}
     
-    corpus+='\n'
+    vals = []
+    
     for val in mud1:
-        corpus+= '\n'
+        corpus+= '\n\n'
         dummy = mud1[val]
+        corpus+=val
+        corpus+=":"
         for damn in dummy:
             corpus+= '\n'
-            print(damn)
-            corpus+= str(dummy[damn])
-        #TODO
+            corpus+= damn
+            corpus+=':  \t'
+            if type(dummy[damn]) == int:
+                corpus+= str(dummy[damn])
+            else:
+                corpus+='P: '
+                corpus+= str(dummy[damn][0])
+                corpus+='\tG: '
+                corpus+= str(dummy[damn][1])
+            vals.append(damn)
+   
+    vals1 = tuple(vals)
+    data[2]['values']=vals1
+    #data[2].current(0)
     
     texto.delete(1.0,END)
     texto.insert(INSERT,corpus)
     
 
 def agregar_obra(data):
-    trial = data[2].get()
+    trial = data[3].get()
     trial1 = trial.replace(' ','')
     if trial1 == '' or trial in data[0]['values']:
         messagebox.showerror('Error al Agregar', 'La obra ya existe o el nombre no es válido.')
@@ -129,16 +163,21 @@ def agregar_obra(data):
         global def_values
         for i in def_values:
             lul.append(i)
-        lul.append(data[2].get())
+        lul.append(data[3].get())
         lul = tuple(lul)
         data[0]['values'] = lul
         def_values = lul
         actualizar_valores(data,'')
 
-def registrar_transaccion():
-    pass
+def registrar_transaccion(data):
+    rta = mu.registrar_transaccion(data[0].get(),data[2].get(),data[3].get())
+    if rta[0] == False:
+        messagebox.showerror('Error al Registrar', rta[1])
+    else:
+        actualizar_valores(data,'')
 
-def ver_reporte():
+
+def agregar_rubro(data):
     pass
 
 
--- front/mockupdata.py
+++ front/mockupdata.py
@@ -5,26 +5,101 @@ Clase de datos falsos para entrega 1
 
 @author: Ditto Castform
 """
-
 import random as r
 
-def generate_mockup(nombre):
+mockups = ''
+
+def generate_mockups(tupla):
+    
+    mus = []
+    
+    for nombre in tupla:
+        mud = {"nombre":nombre}
+    
+        mud['Entradas'] = {}
+        mud['Entradas']['Inversionistas'] = r.randint(1000,10000)*1000000
+        mud['Entradas']['Ventas'] = r.randint(1000,10000)*1000000
+    
+        salidas = {}
+        salidas['Retorno Inversionistas'] =  [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Nomina Empresarial'] =  [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Retorno Inversionistas'] =  [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Obra: Limpieza de Lote'] =  [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Obra: Insumos'] =  [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Obra: Alquiler Maquinaria'] =  [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Escrituras'] = [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Permiso IDU'] = [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Acabados: Cerámica'] = [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        salidas['Acabados: Electrodomésticos'] = [r.randint(10,100)*1000000,r.randint(5,10)*1000000]
+        mud['Salidas'] = salidas
+        mus.append(mud)
+    
+    global mockups
+    mockups = mus
+
+def get_mockup(nombre):
+    
+    global mockups
+    elmu = ''
+    for mu in mockups:
+        if  mu['nombre'] ==  nombre:
+            elmu = mu
+    if elmu == '':
+        huw = generate_new_mockup(nombre)
+        mockups.append(huw)
+        elmu = huw
+    return elmu
+
+def generate_new_mockup(nombre):
     
     mud = {"nombre":nombre}
     
     mud['Entradas'] = {}
-    mud['Entradas']['Inversionistas'] = r.randint(10,100)*1000000
-    mud['Entradas']['Ventas'] = r.randint(10,100)*1000000
+    mud['Entradas']['Inversionistas'] = 0
+    mud['Entradas']['Ventas'] = 0
     
     salidas = {}
-    salidas['Retorno Inversionistas'] =  r.randint(10,100)*1000000
-    salidas['Nomina Empresarial'] =  r.randint(10,100)*1000000
-    salidas['Retorno Inversionistas'] =  r.randint(10,100)*1000000
-    salidas['Stuff'] =  r.randint(10,100)*1000000
-    salidas['Stuff1'] =  r.randint(10,100)*1000000
-    salidas['Ejecución Proyectos'] =  r.randint(10,100)*1000000
-    
-    
+    salidas['Retorno Inversionistas'] = [0,0]
+    salidas['Nomina Empresarial'] =  [0,0]
+    salidas['Retorno Inversionistas'] = [0,0]
+    salidas['Obra: Limpieza de Lote'] = [0,0]
+    salidas['Obra: Insumos'] = [0,0]
+    salidas['Obra: Alquiler Maquinaria'] = [0,0]
+    salidas['Escrituras'] = [0,0]
+    salidas['Permiso IDU'] = [0,0]
+    salidas['Acabados: Cerámica'] = [0,0]
+    salidas['Acabados: Electrodomésticos'] = [0,0]
     mud['Salidas'] = salidas
     
-    return mud
+    return  mud
+
+def registrar_transaccion(nombre,transaccion,monto):
+    eqe = ''
+    print(nombre)
+    print(transaccion)
+    print(monto)
+    for i in mockups:
+        if i['nombre']==nombre:
+            eqe = i
+            break
+    if eqe == '':
+        return (False,'Obra no encontrada')
+    print(eqe)
+    if True:
+        monto = int(monto)
+        if transaccion in eqe['Entradas'].keys():
+            eqe['Entradas'][transaccion]+=monto
+            return (True,'')
+        elif transaccion in eqe['Salidas'].keys():
+            if eqe['Salidas'][transaccion][0]==0:
+                eqe['Salidas'][transaccion][0]=monto
+                return (True,'')
+            elif eqe['Salidas'][transaccion][1] + monto > eqe['Salidas'][transaccion][0]:
+                return (False,'Esta transacción excede el presupuesto')
+            else:
+                eqe['Salidas'][transaccion][1] += monto
+                return (True,'')
+        else:
+            return (False,'Transaccion no válida')
+    else:
+        return(False,'Ingrese por favor un número')
